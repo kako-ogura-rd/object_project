9157f1243d7c2452d66e57b740213de182ff515b
kako_ogura <your.email@example.com>
2020-12-29 15:15:20 +0900

    更新用値を保持する処理

--- update_form.php
+++ update_form.php
@@ -4,10 +4,11 @@
     $blog = new Blog();
     $result = $blog->getById($_GET['id']);
 
+    $id = $result['id'];
     $title = $result['title'];
     $content = $result['content'];
-    $category = $result['category'];
-    $publish_status = $result['publish_status'];
+    $category = (int)$result['category'];
+    $publish_status = (int)$result['publish_status'];
 
 ?>
 <!DOCTYPE html>
@@ -20,23 +21,24 @@
 </head>
 <body>
 <h2>ブログ更新フォーム</h2>
-<form action="blog_create.php" method="POST">
+<form action="blog_update.php" method="POST">
+    <input type="hidden" name="id" value="<?php echo $id?>">
     <p>ブログタイトル</p>
-    <input type="text" name="title">
+    <input type="text" name="title" value="<?php echo $title?>">
     <p>ブログ本文</p>
-    <textarea name="content" id="content" cols="30" rows="10"></textarea>
+    <textarea name="content" id="content" cols="30" rows="10"><?php echo $content?></textarea>
     <br>
     <p>投稿日：</p>
     <input type="date" name="post_at">
     <br>
     <p>カテゴリ</p>
     <select name="category">
-        <option value="1">日常</option>
-        <option value="2">プログラミング</option>
+        <option value="1" <?php if ($category === 1) echo "selected"?>>日常</option>
+        <option value="2" <?php if ($category === 2) echo "selected"?>>プログラミング</option>
     </select>
     <br>
-    <input type="radio" name="publish_status" value="1" checked>公開
-    <input type="radio" name="publish_status" value="2">非公開
+    <input type="radio" name="publish_status" value="1" <?php if ($publish_status === 1) echo "checked"?>>公開
+    <input type="radio" name="publish_status" value="2" <?php if ($publish_status === 2) echo "checked"?>>非公開
     <br>
     <input type="submit" value="送信">
 </form>
